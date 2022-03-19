10e0e926530285080cb09a88a3491f6973c8b3bb
LittleToes <kristina.r.brennan@gmail.com>
2022-03-19 12:51:06 -0500

    remove commas from array and print new password on screen

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=','+',',','.','[',']','{','}','/','~', '"'];
+var passArray = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,9 +13,7 @@ var generateBtn = document.querySelector("#generate");
 var generatePassword = function () {
   var passLength = prompt("Please set password length. You must choose between 8 and 128 characters.");
 
-  var passArray = [];
-
-  if (passLength <8 || passLength > 120) {
+  if (passLength <8 || passLength > 128) {
     prompt("Invalid number. Please choose a length between 8 and 128 characters.");
   }
 
@@ -44,12 +43,40 @@ var generatePassword = function () {
       return generatePassword();
     }
 
+    console.log(passArray)
+
+    var randomizePassArray = function(passArray) {
+      var shuffledArray = passArray.sort((a, b) => 0.5 - Math.random());
+      console.log(shuffledArray);
+    }
+
+    randomizePassArray(passArray);
+
+
+    var addChars = function(passArray, passLength) {
+      completePass = [];
+
+      if (completePass.length < passArray.length) {
+        for (i=0; i<passLength; i++){
+          completePass.push(passArray[i])
+        }
+        completePass = completePass.join("");
+      }
+    
+    }
+
+    addChars(passArray, passLength);
+
+    return completePass;
+
 }
 
+
 // Write password to the #password input
-function writePassword() {
+function writePassword(completePass) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  console.log(completePass);
 
   passwordText.value = password;
 
