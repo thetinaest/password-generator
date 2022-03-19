7b0b5c259a773760660cddd54720efa6032b26ff
LittleToes <kristina.r.brennan@gmail.com>
2022-03-18 22:44:20 -0500

    built arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=','+',',','.','[',']','{','}','/','~'];
 
 
 // Get references to the #generate element
